837c39e3bc6474931c32d06031216feee87a0846
Sudhakar M <sudhakarnan011@gmail.com>
2025-07-09 11:21:31 +0530

    initial push

--- register.php
+++ register.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $stmt->close();
     $conn->close();
 } else {
-    header('Location: register.html');
+    header('Location: registration.html');
     exit;
 }
 ?>
